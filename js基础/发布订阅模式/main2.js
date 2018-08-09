452f351d12488b7432dbd9e35f37c8275fbeec6d
fairysusan <792209917@qq.com>
2018-08-09 15:26:27 +0800

    修改

--- js基础/发布订阅模式/main2.js
+++ js基础/发布订阅模式/main2.js
@@ -1,5 +1,5 @@
 var salesOffices = {};//发布者
-salesOffices.clientList = [];//缓存列表，存放订阅者的回调函数
+salesOffices.clientList = {};//缓存列表，存放订阅者的回调函数
 salesOffices.listen = function(key,sub){
     if(!(this.clientList[key])){
         this.clientList[key] = [];
@@ -17,7 +17,21 @@ salesOffices.trigger = function() {//发布消息
         sub.apply(this,arguments);
     })
 }
-
+//移除订阅者
+salesOffices.removeSubscriber = function(key,sub) { 
+    if(!this.clientList[key] || this.clientList[key].length == 0){
+        return
+    }
+    const index = this.clientList[key].findIndex((element) => {return element == sub});
+    this.clientList[key].splice(index,1);
+}
+/* 移除一类事件的所有订阅者 */
+salesOffices.removeAllSubscribers = function(key) {
+    if(!this.clientList[key] || this.clientList[key].length == 0){
+        return
+    }
+    delete this.clientList[key];
+}
 /* 声明订阅者 */
 function sub1(key,msg) {
     console.log("我是订阅者sub1",key,msg);
@@ -34,6 +48,10 @@ salesOffices.listen('click',sub2);
 salesOffices.listen('mouseover',sub3);
 /* 触发发布 */
 salesOffices.trigger('click',"我是发布的消息");//sub3虽然被订阅，但是不会被执行
+salesOffices.removeSubscriber('click',sub1);
+salesOffices.trigger('click',"我是发布的消息");
+salesOffices.removeAllSubscribers('click');
+salesOffices.trigger('click',"我是发布的消息");
 /* 
 现在发布者发布的消息，全部订阅者都收的到，
 但是接下来我们要实现的是让不同的订阅者接受特定的消息，
